d1e80a5955692ca78953bd56e899ab26e74c0d65
Muhammad Talha Dar <muhammadtalhadar@192.168.1.8>
2022-11-25 21:31:28 +0500

    array_summation refactoring, commentation and completeness

--- array_summation.c
+++ array_summation.c
@@ -16,51 +16,59 @@ void *summation(void *arg) {
   for (int i = details->a; i < details->b; i++) {
     *sum += details->array[i];
   }
+
+  printf("\nTHREAD %d - Sum of Range [%i, %i]: %i", pthread_self(), details->a,
+         details->b, *sum);
+
   pthread_exit(sum);
 }
 
 int main(int argc, char **argv) {
+
+  // doing this takay seg fault se dimaagh kharaab na hou
+  if (argc == 1)
+    exit(EXIT_FAILURE);
+
   int N = atoi(argv[1]);
   int *array = malloc(sizeof(int) * 1000);
 
-  // populate array and display
+  // populate array
   for (int i = 0; i < 1000; i++) {
-    array[i] = rand() % 1000;
-    printf("%i ", array[i]);
+    array[i] = rand() % 10;
   }
-    printf("\n");
 
-  // array of threads to sum each range of the array
+  // create array of threads to sum each range of the array
   pthread_t *threads;
   threads = (pthread_t *)malloc(N * sizeof(pthread_t));
 
   // thread counter
   int count = 0;
 
-  //  data sent to thread
+  //  of data sent to thread
   struct SummationItem *temp = NULL;
 
-  // range of each
+  // width of each summation
   int range = 1000 / N;
 
   for (int i = 0; i < N; i++) {
-    temp = (struct SummationItem*)malloc(sizeof(struct SummationItem));
+    // create a new summation info item
+    temp = (struct SummationItem *)malloc(sizeof(struct SummationItem));
     temp->a = i * range;
-    temp->b = (i+1)*range;
+    temp->b = (i + 1) * range - 1;
     temp->array = array;
 
+    printf("\nRange %i: [%i, %i]", i, temp->a, temp->b);
     pthread_create(&threads[count++], NULL, summation, (void *)(temp));
   }
 
-// get results from all threads
-    int sum =0;
-    void *thread_result;
-    for (int i = 0; i < N; i++)
-    {
-        pthread_join(threads[i], &thread_result);
-        sum+= *(int*)thread_result;
-    }
-    
-    printf("Summation result: %i", sum);
+  // get results from all threads
+  int sum = 0;
+  void *thread_result;
+  for (int i = 0; i < N; i++) {
+    pthread_join(threads[i], &thread_result);
+    sum += *(int *)thread_result;
+  }
+
+  printf("\nSummation result: %i", sum);
   return 0;
 }
